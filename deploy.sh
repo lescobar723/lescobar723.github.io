c0427d334d32441a61d9cd5d6a5462a268e9c392
lescobar <lescobar@umich.edu>
2020-12-31 14:59:37 -0500

    finished deploying

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,9 @@ npm run build
 # navigate into the build output directory
 cd dist
 # if you are deploying to a custom domain
-echo 'www.lesleyescobar.com' > CNAME
+echo 'lesleyescobar.com' > CNAME
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:lescobar723/website-v2.git master:gh-pages
+git push -f git@github.com:lescobar723/lescobar723.github.io.git master
 cd -
